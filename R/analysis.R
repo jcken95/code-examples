2fcb20f214e646c19780c803f1fad0903284f0df
jcken95 <jack_kennedy1@hotmail.co.uk>
2024-11-29 16:13:45 +0000

    chore: add issue number on TODO

--- R/analysis.R
+++ R/analysis.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 
 mtcars |>
-  # TODO: turn into a function which uses the number of cyl as an arg
+  # TODO: turn into a function which uses the number of cyl as an arg (#2)
   filter(cyl == 4) |>
   ggplot(aes(x = wt)) +
   geom_histogram()
+
